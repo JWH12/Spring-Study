01618053063cda5fa4f654a4bc338c083596b9ed
rushk88 <127111182+rushk88@users.noreply.github.com>
2023-07-05 15:04:43 +0900

    1.

---
+++ src/main/java/com/tj/edu/practice2/spring_ioc_practice/Spring_ioc_practice.java
@@ -0,0 +1,10 @@
+package com.tj.edu.practice2.spring_ioc_practice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_ioc_practice {
+    public static void main(String[] args) {
+
+    }
+}
